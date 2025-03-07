abc24352d9a62a66a8299d86bd4ad75f0778a70b
Alice Smith <alice@webage.com>
2025-03-07 14:37:41 -0500

    tim test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import Header from './Header';
-import Footer from './Footer';
-import QuackFeed from './QuackFeed';
-import NewQuackForm from './NewQuackForm';
-import LoginForm from './LoginForm';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import QuackFeed from "./QuackFeed";
+import NewQuackForm from "./NewQuackForm";
+import LoginForm from "./LoginForm";
+import RequireAuth from "./RequireAuth";
+import { AuthProvider } from "./hooks/AuthContext";
 function App() {
   const [quacks, setQuacks] = useState([]);
   useEffect(() => {
     const fetchQuacks = async function () {
-      const response = await fetch('http://localhost:5050/api/quacks');
+      const response = await fetch("http://localhost:5050/api/quacks");
       const data = await response.json();
       setQuacks(data);
     };
@@ -17,10 +20,10 @@ function App() {
   }, []);
 
   const addQuack = async function (newQuack) {
-    const response = await fetch('http://localhost:5050/api/quacks', {
-      method: 'POST',
+    const response = await fetch("http://localhost:5050/api/quacks", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(newQuack),
     });
@@ -28,19 +31,23 @@ function App() {
   };
 
   return (
-    <>
-      <Header />
-      <div className="row">
-        <div className="col-md-8">
-          <QuackFeed quacks={quacks} />
-        </div>
-        <div className="col-md-4">
-          <NewQuackForm quacks={quacks} addQuack={addQuack} />
-          <LoginForm />
-        </div>
-      </div>
-      <Footer />
-    </>
+    <AuthProvider>
+      <Router>
+        <>
+          <Header />
+          <div className="row">
+            <div className="col-md-8">
+              <QuackFeed quacks={quacks} />
+            </div>
+            <div className="col-md-4">
+              <NewQuackForm quacks={quacks} addQuack={addQuack} />
+              <LoginForm />
+            </div>
+          </div>
+          <Footer />
+        </>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,39 @@
-import pkg from 'mongoose';
+import pkg from "mongoose";
+import dotenv from "dotenv";
+import { MongoClient, ObjectId } from "mongodb";
+import cors from "cors";
 const { connect } = pkg;
 
-import app from './app.js';
+import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
+dotenv.config();
+const url = process.env.MONGODB_URI;
+const dbName = process.env.MONGO_DB;
+const collectionName = process.env.MONGO_DB_COLLECTION;
 
 connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
   .then(() => {
-    console.log('Connected to MongoDB');
+    app.get("/quacks", async (req, res) => {
+      try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const socks = await collection.find({}).toArray();
+        res.json(socks);
+      } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+      }
+    });
+    console.log("Connected to MongoDB");
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
-    console.error('MongoDB connection error:', err);
+    console.error("MongoDB connection error:", err);
   });
